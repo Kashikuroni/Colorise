f93b03903fc61cad0f4d8d62f80fe42c95b87590
Nikita Tarasov <Owlcompany@icloud.com>
2022-11-10 23:32:41 +0300

    Colors reset Add all Outlet and action

--- Colorise/ViewController.swift
+++ Colorise/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorPreview: UIView!
+    
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorPreview.layer.cornerRadius = 20
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
     }
 
-
+    @IBAction func redSliderAction() {
+        redValue.text = String(round(redSlider.value*100)/100)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(round(greenSlider.value*100)/100)
+    }
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(round(blueSlider.value*100)/100)
+    }
 }
 
